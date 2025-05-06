6d381a47054518643078a1ee24406978924c5858
Sujal Chauhan <chauhansujal1107@gmail.com>
2025-05-06 22:21:00 +0530

    inital commit

---
+++ backend/src/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+dotenv.config();
+import app from "./app";
+import {Request, Response} from "express";
+import { success } from "./utils/response";
+
+
+const PORT = process.env.PORT || 3000;
+
+
+app.get("/", (req: Request, res: Response) => {
+    const response = success("Welcome to the Server", 200);
+    res.status(response.status).json(response);
+}
+);
+
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+})
